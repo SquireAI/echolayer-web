897ffd43fec73003c34a455e0f093662cb07ba45
Derrick Persson <derrick.persson@usecodex.com>
2023-06-22 12:09:01 -0700

    Fixing telemetry (#16)

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,7 @@ import { PUBLIC_BASE_API_URL } from '$env/static/public';
 import type { HandleServerError } from '@sveltejs/kit';
 
 export const handleError = (async ({ error, event }) => {
-    fetch(`${PUBLIC_BASE_API_URL}/telemetry/exception`, {
+    fetch(`${PUBLIC_BASE_API_URL}telemetry/exception`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
