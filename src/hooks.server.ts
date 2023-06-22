2ea8a49e3ef2c9405bde714988c1d86c0c085513
Derrick Persson <derrick.persson@usecodex.com>
2023-06-22 10:47:21 -0700

    Adding telemetry logging via our API server

---
+++ src/hooks.server.ts
@@ -0,0 +1,22 @@
+import { PUBLIC_BASE_API_URL } from '$env/static/public';
+import type { HandleServerError } from '@sveltejs/kit';
+
+export const handleError = (async ({ error, event }) => {
+    fetch(`${PUBLIC_BASE_API_URL}/telemetry/exception`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ 
+            error: {
+              error,
+              event,
+            },
+            source: "sveltekit"
+        }),
+    })
+
+    return {
+        message: 'Whoops!',
+    };
+}) satisfies HandleServerError;
