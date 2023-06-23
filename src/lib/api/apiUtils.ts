4cd6dcda0eea88f9df6fadc282c637b40384dbf2
Chris Pulsinelli <chris@usecodex.com>
2023-06-23 12:30:08 -0400

    conditional credentials header if client side request

--- src/lib/api/apiUtils.ts
+++ src/lib/api/apiUtils.ts
@@ -1,4 +1,5 @@
 import { PUBLIC_BASE_API_URL } from "$env/static/public"
+import { browser } from "$app/environment";
 import type { Cookies } from "@sveltejs/kit";
 export const BASE_API_URL: string = PUBLIC_BASE_API_URL || "http://localhost:3005";
 
@@ -126,7 +127,7 @@ const config = (
 ): RequestInit => {
 	return {
 		method,
-		// credentials: "include", // needed to send cookies to only our server
+		...( browser && { credentials: "include" }), // needed to send cookies to only our server
 		headers: {
 			"Content-Type": "application/json",
 			...(headers !== undefined && headers)
