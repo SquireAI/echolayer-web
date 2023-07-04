0321de617518bf227a263290e660862608c08a8f
Chris Pulsinelli <chris@usecodex.com>
2023-07-04 16:16:30 -0400

    New discovery page with svelvet graph library

---
+++ src/lib/discovery/canvas.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Background, Node, Svelvet } from "svelvet";
+</script>
+
+<Svelvet>
+	<Node />
+	<Background dotColor="#D4D4D4" bgColor="transparent" gridWidth={30} dotSize={3} slot="background" />
+</Svelvet>
---
+++ src/routes/(discovery)/discovery/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Canvas from "$lib/discovery/canvas.svelte";
+</script>
+
+<Canvas />
