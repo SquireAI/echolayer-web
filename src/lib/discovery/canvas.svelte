028d71b21e4331f068140d6313edc61363d0ba57
Chris Pulsinelli <chris@usecodex.com>
2023-07-05 16:25:31 -0400

    Added component entity node. Added sample data.

--- src/lib/discovery/canvas.svelte
+++ src/lib/discovery/canvas.svelte
@@ -2,10 +2,12 @@
 	import { Background, Svelvet } from "svelvet";
 	import TeamComponentNode from "./components/TeamComponentNode.svelte";
 	import { data } from "./sample-data";
+	import ComponentNode from "./components/ComponentNode.svelte";
 </script>
 
 <Svelvet zoom={1} fixedZoom={true} theme="echolayer">
 	<TeamComponentNode component={data.teams[0]} origin={{ x: 20, y: 20}} />
 	<TeamComponentNode component={data.teams[1]} origin={{ x: 280, y: 20 }} />
+	<ComponentNode title="New Component node" --node-border-radius="10px" />
 	<Background dotColor="#D4D4D4" bgColor="transparent" gridWidth={30} dotSize={3} slot="background" />
 </Svelvet>
