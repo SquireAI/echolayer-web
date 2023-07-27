fc06e1feba71d30c9c91d9cea3bddc55937aab44
Meichen Dong <meichen.dong@usecodex.com>
2023-07-27 10:12:43 -0400

    prop

--- src/lib/discovery/components/details/OriginTag.svelte
+++ src/lib/discovery/components/details/OriginTag.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import PointScan from "$lib/svgs/PointScan.svg?component";
+	export let isHidden: boolean;
 </script>
-<div class="origin-tag flex justify-center gap-1 items-center text-xs absolute -top-7 bg-echolayer-green-200/20 text-echolayer-green rounded-full h-5 w-[69px] font-sans">
+<div class={`flex justify-center gap-1 items-center text-xs bg-echolayer-green-200/20 text-echolayer-green rounded-full h-5 w-[69px] font-sans ${isHidden ? "hidden" : ""}`}>
 	<PointScan />
 	Origin
 </div>
