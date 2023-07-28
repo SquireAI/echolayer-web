02cb81dcbf7fab2049a2a051c5b48dc905bf7981
Meichen Dong <meichen.dong@usecodex.com>
2023-07-28 15:00:37 -0400

    Render actions in detal page with links

--- src/lib/discovery/components/details/DetailsButton.svelte
+++ src/lib/discovery/components/details/DetailsButton.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     export let label: string = "";
     export let onClick: () => void = () => {};
+    export let href: string = "";
+    export let target: string = "";
 </script>
 
 <style lang="scss">
@@ -14,7 +16,16 @@
     }
 </style>
 
-<button class="button" on:click={onClick}>
-    <slot name="icon"></slot>
-    {label}
-</button>
+{#if !href}
+    <button class="button" on:click={onClick}>
+        <slot name="icon"></slot>
+        {label}
+    </button>
+{:else}
+	<a href={href} class="button" data-sveltekit-preload-data="hover" on:click={onClick} target={target}>
+        <slot name="icon"></slot>
+        {label}
+    </a>
+{/if}
+
+
--- src/lib/discovery/components/details/DetailsTitle.svelte
+++ src/lib/discovery/components/details/DetailsTitle.svelte
@@ -1,5 +1,6 @@
 <script>
     import DetailsButton from "$lib/discovery/components/details/DetailsButton.svelte";
+	import { entityDetailsStore } from "$lib/stores";
     import OpenInNew from "svelte-material-icons/OpenInNew.svelte";
 </script>
 
@@ -26,12 +27,13 @@
         <span class="text-neutral-500 text-xs">Created 2023</span>
     </div>
 
-    <div class="actions">
-        <DetailsButton label="Main Channel">
-            <span slot="icon"><OpenInNew width={15} height={15} color="#2654F4" /></span>
-        </DetailsButton>
-        <DetailsButton label="Message DRI">
-            <span slot="icon"><OpenInNew width={15} height={15} color="#2654F4" /></span>
-        </DetailsButton>
-    </div>
+    {#if $entityDetailsStore.entity?.links?.length}
+        <div class="actions">
+            {#each $entityDetailsStore.entity.links as link}
+                <DetailsButton label={link.name[0].toUpperCase() + link.name.substring(1).toLowerCase()} href={link.url} target="_blank">
+                    <span slot="icon"><OpenInNew width={15} height={15} color="#2654F4" /></span>     
+                </DetailsButton>
+            {/each}
+        </div>
+    {/if}
 </div>
