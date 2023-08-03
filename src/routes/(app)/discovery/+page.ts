ff55aa55cef1aab1759d4204ec2343efb5e4d1f4
Meichen Dong <meichen.dong@usecodex.com>
2023-08-02 20:24:22 -0400

    explicit depth

--- src/routes/(app)/discovery/+page.ts
+++ src/routes/(app)/discovery/+page.ts
@@ -19,7 +19,7 @@ export const load = (async ({ data, fetch, parent }) => {
 	const context = createDefaultContext(fetch, baseHeaders, baseUrl);
 
 	const getRelationsGraph = async (origin: BaseEntity): Promise<RelationGraphEntity[]> => {
-		const relations = await new RelationsGraphApi(context).list({ sourcePublicId: origin?.publicId })
+		const relations = await new RelationsGraphApi(context).list({ sourcePublicId: origin?.publicId, depth: 1 })
 		return relations.filter(relation => relation.relationshipName !== "hasMember") || [];
 	}
 
