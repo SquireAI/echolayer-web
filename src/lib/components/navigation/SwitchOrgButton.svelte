41f584fc77a2f425b329db4bce0df581d65f5ea6
Karl Clement <k@echolayer.com>
2023-08-14 16:26:32 -0400

    Last icon

--- src/lib/components/navigation/SwitchOrgButton.svelte
+++ src/lib/components/navigation/SwitchOrgButton.svelte
@@ -5,7 +5,7 @@
 	import type { SelectedOrganizationStore } from '$lib/types';
 	import { ORGS_SELECT_PATH } from '$lib/utils/paths';
 	import { getContext } from 'svelte';
-	import SwapHorizontal from 'svelte-material-icons/SwapHorizontal.svelte';
+	import SwapHorizontalIcon from 'svelte-material-icons/SwapHorizontal.svelte';
 
 	const orgStore = getContext(SELECTED_ORG_STORE_NAME) as SelectedOrganizationStore;
 
@@ -36,7 +36,7 @@
 				<p class="font-medium leading-5">Select organization...</p>
 			{/if}
 		</span>
-		<SwapHorizontal
+		<SwapHorizontalIcon
 			height="24"
 			width="24"
 			class={`
