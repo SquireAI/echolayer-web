d2ac754186218fd8b9b44459071ec61de2392670
David Sullivan <david.sullivan@usecodex.com>
2023-08-31 14:53:46 -0400

    Revert "ENG-919: Adding logging for each request to init with DD and adding a trace ID to all fetch calls to our API (#114)"
    This reverts commit 1ee9d3c29e7459d33fae188a7a8e3845ee332863.

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,13 +3,9 @@
 declare global {
 	namespace App {
 		// interface Error {}
+		// interface Locals {}
 		// interface PageData {}
 		// interface Platform {}
-
-		interface Locals {
-			traceId: string;
-			orgId: string | null;
-		}
 	}
 }
 
