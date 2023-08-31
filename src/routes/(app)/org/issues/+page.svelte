bd261c233a1bd3cf7eec05615f5718e0b255a9ed
davidscodex <98758039+davidscodex@users.noreply.github.com>
2023-08-31 15:03:26 -0400

    Revert logging with pino (#126)
    * Allow unknown components in issues page
    
    * Revert "ENG-919: Adding logging for each request to init with DD and adding a trace ID to all fetch calls to our API (#114)"
    
    This reverts commit 1ee9d3c29e7459d33fae188a7a8e3845ee332863.

--- src/routes/(app)/org/issues/+page.svelte
+++ src/routes/(app)/org/issues/+page.svelte
@@ -1,61 +1,60 @@
 <script lang="ts">
-	import { page } from "$app/stores";
-	import Navigation from "$lib/components/navigation/Navigation.svelte";
-	import TabSwitch from "$lib/components/tabs/TabSwitch.svelte";
-	import PanelsHeader from "$lib/discovery/components/PanelsHeader.svelte";
-	import Panels from "$lib/discovery/panels.svelte";
-	import type { Issue } from "$lib/types";
+	import { page } from '$app/stores';
+	import Navigation from '$lib/components/navigation/Navigation.svelte';
+	import TabSwitch from '$lib/components/tabs/TabSwitch.svelte';
+	import PanelsHeader from '$lib/discovery/components/PanelsHeader.svelte';
+	import Panels from '$lib/discovery/panels.svelte';
+	import type { Issue } from '$lib/types';
 
-    let issues: Issue[] = [];
-    $: issues = $page.data?.issues || [];
+	let issues: Issue[] = [];
+	$: issues = $page.data?.issues || [];
 
-    let tabs = ['unresolved', 'resolved'];
-    let selected = 0;
+	let tabs = ['unresolved', 'resolved'];
+	let selected = 0;
 
-    let filteredIssues: Issue[] = [];
-    $: (
-        selected, 
-        filteredIssues = issues.filter((issue: Issue) => selected === 0 ? !issue.resolved : issue.resolved)
-    )
+	let filteredIssues: Issue[] = [];
+	$: selected,
+		(filteredIssues = issues.filter((issue: Issue) =>
+			selected === 0 ? !issue.resolved : issue.resolved
+		));
 </script>
-
 
 <Panels>
 	<Navigation slot="nav" />
-    <div slot="content" class="bg-white h-full">
-        <PanelsHeader title="EchoLayer Issues">
-            <div slot="right-action">
-                <div class="flex-1">
-					<TabSwitch {tabs} bind:selected={selected} />
+	<div slot="content" class="bg-white h-full">
+		<PanelsHeader title="EchoLayer Issues">
+			<div slot="right-action">
+				<div class="flex-1">
+					<TabSwitch {tabs} bind:selected />
 				</div>
-            </div>
-        </PanelsHeader>
+			</div>
+		</PanelsHeader>
 
-        <table class="w-full table-fixed text-neutral-500">
-            <thead>
-                <tr class="border-b border-neutral-300 p-2 pl-6 w-full uppercase text-left font-normal">
-                    <th class="pl-6 font-normal">Entity</th>
-                    <th class="font-normal">Issue Created</th>
-                    <th class="font-normal">Reason</th>
-                </tr>
-            </thead>
-            <tbody>
-                {#if filteredIssues.length === 0}
-                    <tr>
-                        <td colspan="3" class="text-center p-8">No issues found!</td>
-                    </tr>
-                {:else}
-                    {#each filteredIssues as issue (issue.publicId)}
-                        <tr class="w-fullgap-2 border-l-2 border-neutral-300 group hover:bg-neutral-100 ">
-                            <td class="py-2 pl-5">{issue.component.name}</td>
-                            <td class="py-2">{new Date(issue.createdAt).toDateString()}</td>
-                            <td class="py-2">{issue.description}</td>
-                        </tr>
-                        <!-- Spacing element -->
-                        <tr><td class="h-1"></td></tr>
-                    {/each}
-                {/if}
-            </tbody>
-        </table>
-    </div>
+		<table class="w-full table-fixed text-neutral-500">
+			<thead>
+				<tr class="border-b border-neutral-300 p-2 pl-6 w-full uppercase text-left font-normal">
+					<th class="pl-6 font-normal">Entity</th>
+					<th class="font-normal">Issue Created</th>
+					<th class="font-normal">Reason</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#if filteredIssues.length === 0}
+					<tr>
+						<td colspan="3" class="text-center p-8">No issues found!</td>
+					</tr>
+				{:else}
+					{#each filteredIssues as issue (issue.publicId)}
+						<tr class="w-fullgap-2 border-l-2 border-neutral-300 group hover:bg-neutral-100">
+							<td class="py-2 pl-5">{issue.component?.name}</td>
+							<td class="py-2">{new Date(issue.createdAt).toDateString()}</td>
+							<td class="py-2">{issue.description}</td>
+						</tr>
+						<!-- Spacing element -->
+						<tr><td class="h-1" /></tr>
+					{/each}
+				{/if}
+			</tbody>
+		</table>
+	</div>
 </Panels>
