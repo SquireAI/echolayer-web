893ebab7e62122a6218744d5ecc0bfab33adaa93
Derrick Persson <derrick.persson@usecodex.com>
2023-09-05 15:39:59 -0700

    Adding new sign-in page (#128)

---
+++ src/lib/layouts/open/OpenLayout.svelte
@@ -0,0 +1,26 @@
+<script>
+	import './OpenLayout.scss';
+	import Header from '$lib/Header.svelte';
+</script>
+
+<div class="app-container">
+	<div
+		class="app-container--left bg-cover bg-center hidden md:block md:absolute top-0 left-0 lg:left-[10%] w-[500px] h-[290px]"
+		style="background-image: url('/assets/echoLayer_graphic_left.png')"
+	/>
+	<div
+		class="app-container--right bg-cover bg-center hidden md:block md:absolute bottom-0 right-0 lg:right-[10%] w-[500px] h-[290px]"
+		style="background-image: url('/assets/echoLayer_graphic_right.png')"
+	/>
+	<header>
+		<Header />
+	</header>
+
+	<main>
+		<div class="main__content--left" />
+		<div class="main__content">
+			<slot />
+		</div>
+		<div class="main__content--right" />
+	</main>
+</div>
