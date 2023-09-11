c386f8393e1ed665237692a1d328dde5ce522d41
Derrick Persson <derrick.persson@usecodex.com>
2023-09-11 16:02:29 -0700

    Cloudflare adapters trigger auth hook redirect, check for building env to avoid auth check

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,15 @@
+import { building } from '$app/environment';
 import { getHttpContext } from '$lib/http/context';
 import { authRequired, orgRequired } from '$lib/utils/access';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
+	// Workaround due to: https://github.com/sveltejs/kit/issues/9386
+	if (building) {
+		const response = await resolve(event);
+		return response;
+	}
+
 	const context = getHttpContext(event.fetch, event.cookies);
 
 	if (event.url.pathname.startsWith('/sign-in')) {
