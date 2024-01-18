5e34bb8625e92383a665a5b2b10c344f6f286280
Karl Clement <k@echolayer.com>
2024-01-18 12:50:26 -0800

    Fix for full onboarding from start (#164)

--- src/routes/(app)/onboarding/org/+page.server.ts
+++ src/routes/(app)/onboarding/org/+page.server.ts
@@ -1,28 +1,24 @@
 import { RepositoryApi } from '$lib/api/repository';
 import { getHttpContext, type httpContext } from '$lib/http/context';
 import type { Organization, Repository } from '$lib/types';
-import { orgRequired } from '$lib/utils/access';
+import { orgOptional } from '$lib/utils/access';
+import { ONBOARDING_PATH } from '$lib/utils/paths';
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from '../$types';
 
 export type PageData = {
 	baseHeaders: httpContext['baseHeaders'];
 	baseUrl: httpContext['baseUrl'];
-	org: Organization;
-	repositories: Repository[];
+	org?: Organization;
 };
 
 export const load = (async ({ cookies, fetch, params }): Promise<PageData> => {
 	const context = getHttpContext(fetch, cookies);
 	const { baseHeaders, baseUrl } = context;
 
-	const org = await orgRequired(context);
-	const repositoryApi = new RepositoryApi(context);
-	const repositories = await repositoryApi.list();
+	// Optionally setup org
+	const org = await orgOptional(context);
+	if (org) throw redirect(307, `${ONBOARDING_PATH}/github`);
 
-	return {
-		baseHeaders,
-		baseUrl,
-		org,
-		repositories
-	};
+	return { baseHeaders, baseUrl };
 }) satisfies PageServerLoad;
